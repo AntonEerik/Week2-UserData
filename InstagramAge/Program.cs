bbe314ccd1127453447460d7be1668b76ad4e31c
Anton Eerik Pauts <anton.eerik.pauts@gmail.com>
2021-09-15 16:53:13 +0300

    Control Flow

---
+++ InstagramAge/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace InstagramAge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm küsib kasutaja vanust;
+            //programm kontrollib, kas kasutaja on vanem kui 13 või 13 aastat vana;
+            //kui kasutaja on 13 või vanem, sisi ta võib Instagram-i kasutada;
+            //kui kasutaja on noorem, kui 13, siis ta on liiga noor;
+
+            Console.WriteLine("Palun sisesta oma vanus:");
+            int userAge = Convert.ToInt32(Console.ReadLine());
+
+            if (userAge >= 13)
+            {
+                Console.WriteLine("Oled piisavalt vana, et Instagram-i kontot luua.");
+            }
+            else
+            {
+                Console.WriteLine("Oled liga noor, et Instagram-i kontot luua");
+            }
+            Console.WriteLine("Kena päeva!");
+        }
+    }
+}
---
+++ UserAge/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace UserAge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm küsib kasutaja vanust;
+            //programm arvutab kasutaja sünniaastat;
+            //programm kuvab sünniaasta konsoolis;
+            //"oled sündinud aastal {yearOfBirth}"
+            Console.WriteLine("Palun sisesta oma vanus:");
+            int UserAge = Convert.ToInt32(Console.ReadLine());
+
+            int yearOfBirth = 2021 - UserAge;
+
+            Console.WriteLine($"Oled sündinud {yearOfBirth} aastal.");
+        }
+    }
+}
